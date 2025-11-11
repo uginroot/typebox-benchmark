f29ce84972d4147c7c688642c26689807d5099cf
Soroka Anton <uginroot@gmail.com>
2025-11-11 09:08:48 +0600

    enhance benchmark tests with detailed object schemas and custom decoding

--- README.md
+++ README.md
@@ -12,20 +12,58 @@ const schemaWithoutCodec = Type.Object({
 
 ### Result 100000 iterations with tsx
 
-| (index) | name                   | duration (ms) | ops/sec   |
-|---------|------------------------|---------------|-----------|
-| 0       | 'check without codec'  | 1.58          | 63392125  |
-| 1       | 'check with codec'     | 1.63          | 61248314  |
-| 2       | 'decode without codec' | 0.94          | 106387845 |
-| 3       | 'decode with codec'    | 5697.65       | 17551     |
+#### Benchmarking test: Small Object
+
+| (index) | name                                 | duration (ms) | ops/sec   |
+|---------|--------------------------------------|---------------|-----------|
+| 0       | 'Compile check without codec'        | 1.77          | 56460301  |
+| 1       | 'Compile check with codec'           | 1.95          | 51404552  |
+| 2       | 'Compile decode without codec'       | 1.22          | 81791161  |
+| 3       | 'Compile decode with codec'          | 5785.03       | **17286** |
+| 4       | 'Value.Check check without codec'    | 1616.25       | 61872     |
+| 5       | 'Value.Check check with codec'       | 1658.02       | 60313     |
+| 6       | 'CustomDecoder decode without codec' | 2363.82       | 42304     |
+| 7       | 'CustomDecoder decode with codec'    | 2224.29       | 44958     |
+
+#### Benchmarking test: Big Object
+| (index) | name                                 | duration (ms) | ops/sec  |
+|---------|--------------------------------------|---------------|----------|
+| 0       | 'Compile check without codec'        | 61.21         | 1633850  |
+| 1       | 'Compile check with codec'           | 60.9          | 1641974  |
+| 2       | 'Compile decode without codec'       | 57.56         | 1737339  |
+| 3       | 'Compile decode with codec'          | 74228.67      | **1347** |
+| 4       | 'Value.Check check without codec'    | 13066.99      | 7653     |
+| 5       | 'Value.Check check with codec'       | 12864.36      | 7773     |
+| 6       | 'CustomDecoder decode without codec' | 21677.29      | 4613     |
+| 7       | 'CustomDecoder decode with codec'    | 22194.07      | 4506     |
+
 
 ### Result 100000 iterations with node
 
+#### Benchmarking test: Small Object
 
-| (index) | name                   | duration (ms) | ops/sec  |
-|---------|------------------------|---------------|----------|
-| 0       | 'check without codec'  | 1.65          | 60484871 |
-| 1       | 'check with codec'     | 1.63          | 61356394 |
-| 2       | 'decode without codec' | 1.01          | 98885266 |
-| 3       | 'decode with codec'    | 417.85        | 239322   |
+| (index) | name                                 | duration (ms) | ops/sec    |
+|---------|--------------------------------------|---------------|------------|
+| 0       | 'Compile check without codec'        | 1.75          | 57235284   |
+| 1       | 'Compile check with codec'           | 1.85          | 53911088   |
+| 2       | 'Compile decode without codec'       | 1.17          | 85527542   |
+| 3       | 'Compile decode with codec'          | 431.66        | **231662** |
+| 4       | 'Value.Check check without codec'    | 100.37        | 996342     |
+| 5       | 'Value.Check check with codec'       | 97.19         | 1028952    |
+| 6       | 'CustomDecoder decode without codec' | 156.57        | 638696     |
+| 7       | 'CustomDecoder decode with codec'    | 156.36        | 639555     |
+
+
+#### Benchmarking test: Big Object
+
+| (index) | name                                 | duration (ms) | ops/sec   |
+|---------|--------------------------------------|---------------|-----------|
+| 0       | 'Compile check without codec'        | 56.92         | 1757002   |
+| 1       | 'Compile check with codec'           | 52.06         | 1920843   |
+| 2       | 'Compile decode without codec'       | 52.02         | 1922312   |
+| 3       | 'Compile decode with codec'          | 5509.57       | **18150** |
+| 4       | 'Value.Check check without codec'    | 986.42        | 101377    |
+| 5       | 'Value.Check check with codec'       | 981.71        | 101864    |
+| 6       | 'CustomDecoder decode without codec' | 1742.89       | 57376     |
+| 7       | 'CustomDecoder decode with codec'    | 1579.76       | 63301     |
 
--- src/benchmark.ts
+++ src/benchmark.ts
@@ -1,26 +1,111 @@
 import {Type} from 'typebox';
-import {Compile} from 'typebox/compile';
+import {Compile, Validator} from 'typebox/compile';
+import {Value, Pipeline, DecodeUnsafe, Assert} from 'typebox/value';
 
-const schemaWithCodec = Type.Object({
-    name: Type.Codec(Type.String())
-        .Decode((value) => value)
-        .Encode((value) => value),
-});
-const validatorWithCodec = Compile(schemaWithCodec);
+type Test = {
+    name: string;
+    schemaWithCodec: Type.TSchema;
+    schemaWithoutCodec: Type.TSchema;
+    validatorWithCodec: Validator;
+    validatorWithoutCodec: Validator;
+    testData: object;
+}
 
-const schemaWithoutCodec = Type.Object({
-    name: Type.String(),
-});
-const validatorWithoutCodec = Compile(schemaWithoutCodec);
+const iterations = 100_000;
+
+const tests: Test[] = [
+    (() => {
+        const schemaWithCodec = Type.Object({
+            name: Type.Codec(Type.String())
+                .Decode((value) => value)
+                .Encode((value) => value),
+        });
+        const schemaWithoutCodec = Type.Object({
+            name: Type.String(),
+        });
+        return {
+            name: 'Small Object',
+            schemaWithCodec,
+            schemaWithoutCodec,
+            validatorWithCodec: Compile(schemaWithCodec),
+            validatorWithoutCodec: Compile(schemaWithoutCodec),
+            testData: {
+                name: 'John Doe',
+            },
+        }
+    })(),
+    (() => {
+        const FEWidgetCustomizationResolvedEventMessagePayloadSchema = Type.Object({
+            widgetName: Type.String()
+        });
 
 
-const testData = {
-    name: 'John Doe',
-};
+        const BaseFEEventHeadersSchema = Type.Object({
+            dateTime: Type.Codec(Type.String({ format: "date-time" })).
+                Decode((value: string) => value).
+                Encode((value: string) => value),
+            userAgent: Type.String(),
+            location: Type.String(),
+            app: Type.String(),
+            shopId: Type.Number(),
+            sessionToken: Type.Union([Type.String(), Type.Null()]),
+            customerId: Type.Union([Type.Number(), Type.Null()]),
+            ipAddress: Type.Union([Type.String({ format: "ipv4" }), Type.String({ format: "ipv6" })])
+        });
+
+
+        const BaseFEEventHeadersSchemaWithoutCodec = Type.Object({
+            dateTime: Type.String({ format: "date-time" }),
+            userAgent: Type.String(),
+            location: Type.String(),
+            app: Type.String(),
+            shopId: Type.Number(),
+            sessionToken: Type.Union([Type.String(), Type.Null()]),
+            customerId: Type.Union([Type.Number(), Type.Null()]),
+            ipAddress: Type.Union([Type.String({ format: "ipv4" }), Type.String({ format: "ipv6" })])
+        });
+
+        const schemaWithCodec = Type.Object({
+            type: Type.Literal("FE.WidgetCustomizationResolved"),
+            source: Type.String(),
+            headers: BaseFEEventHeadersSchema,
+            payload: FEWidgetCustomizationResolvedEventMessagePayloadSchema
+        });
+        const schemaWithoutCodec = Type.Object({
+            type: Type.Literal("FE.WidgetCustomizationResolved"),
+            source: Type.String(),
+            headers: BaseFEEventHeadersSchemaWithoutCodec,
+            payload: FEWidgetCustomizationResolvedEventMessagePayloadSchema
+        });
+        return {
+            name: 'Big Object',
+            schemaWithCodec: schemaWithCodec,
+            schemaWithoutCodec: schemaWithoutCodec,
+            validatorWithCodec: Compile(schemaWithCodec),
+            validatorWithoutCodec: Compile(schemaWithoutCodec),
+            testData: {
+                "type": "FE.WidgetCustomizationResolved",
+                "source": "frontend-app",
+                "headers": {
+                    "dateTime": "2025-10-10T12:00:00Z",
+                    "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
+                    "location": "https://example.com/shop",
+                    "app": "default",
+                    "shopId": 12345,
+                    "sessionToken": "abc123xyz",
+                    "customerId": 67890,
+                    "ipAddress": "192.168.1.1"
+                },
+                "payload": {
+                    "widgetName": "product-carousel"
+                }
+            },
+        }
+    })(),
+];
 
 
 function benchmark(name: string, fn: () => void): { name: string; "duration (ms)": number; "ops/sec": number } {
-    const iterations = 100_000;
     const start = performance.now();
 
     for (let i = 0; i < iterations; i++) {
@@ -34,10 +119,27 @@ function benchmark(name: string, fn: () => void): { name: string; "duration (ms)
     return { name, "duration (ms)": Number(duration.toFixed(2)), "ops/sec": opsSec };
 }
 
-console.table([
-    benchmark("check without codec", () => validatorWithoutCodec.Check(testData)),
-    benchmark("check with codec", () => validatorWithCodec.Check(testData)),
-    benchmark("decode without codec", () => validatorWithoutCodec.Decode(testData)),
-    benchmark("decode with codec", () => validatorWithCodec.Decode(testData)),
-]);
+const CustomDecoder = Pipeline([
+    // tip: replace Assert with Compiled Type Assert for even faster performance.
+    (context, type, value) => { Assert(context, type, value); return value },
+    (context, type, value) => DecodeUnsafe(context, type, value)
+])
+
+function runTest(test: Test) {
+    console.log(`\nBenchmarking test: ${test.name}`);
+    console.table([
+        benchmark("Compile check without codec", () => test.validatorWithoutCodec.Check(test.testData)),
+        benchmark("Compile check with codec", () => test.validatorWithCodec.Check(test.testData)),
+        benchmark("Compile decode without codec", () => test.validatorWithoutCodec.Decode(test.testData)),
+        benchmark("Compile decode with codec", () => test.validatorWithCodec.Decode(test.testData)),
+        benchmark("Value.Check check without codec", () => Value.Check(test.schemaWithoutCodec, test.testData)),
+        benchmark("Value.Check check with codec", () => Value.Check(test.schemaWithCodec, test.testData)),
+        benchmark("CustomDecoder decode without codec", () => CustomDecoder(test.schemaWithoutCodec, test.testData)),
+        benchmark("CustomDecoder decode with codec", () => CustomDecoder(test.schemaWithCodec, test.testData)),
+    ]);
+}
+
+for (const test of tests) {
+    runTest(test);
+}
 
